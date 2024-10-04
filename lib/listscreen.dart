06e681b2c12fb594df6fbe40863293da4b62379b
Saicharan-R25 <saicharan.r2003@gmail.com>
2024-10-04 08:17:16 +0530

    created dummy list to know workflow

--- lib/listscreen.dart
+++ lib/listscreen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 class Listscreen extends StatelessWidget {
-  const Listscreen({Key? key}) : super(key: key);
-
+  Listscreen({Key? key}) : super(key: key);
+  List<String> testing=["one","two","three"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("List Operations Demo"),),
-      body: Center(child: Text("your list goes here"),),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: ListView.builder(
+          itemCount: testing.length,
+          itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(testing[index]),
+          );
+        },),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: Icon(Icons.add),
